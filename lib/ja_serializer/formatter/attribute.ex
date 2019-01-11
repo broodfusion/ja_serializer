17a27167e084ace693c7d8facd734e38490ad9a0
Shawn Liang <broodfusion@gmail.com>
2019-01-10 16:01:32 -0800

    add formatting for nested attrs

--- lib/ja_serializer/formatter/attribute.ex
+++ lib/ja_serializer/formatter/attribute.ex
@@ -1,6 +1,11 @@
 defimpl JaSerializer.Formatter, for: JaSerializer.Builder.Attribute do
   alias JaSerializer.Formatter.Utils
 
+  def format(%{value: value, key: key}) when is_map(value) or is_list(value) do
+    values = Utils.deep_format_keys(value)
+    {Utils.format_key(key), JaSerializer.Formatter.format(values)}
+  end
+
   def format(attr) do
     {Utils.format_key(attr.key), JaSerializer.Formatter.format(attr.value)}
   end
